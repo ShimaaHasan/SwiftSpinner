9597e21ff2c4bbb7f31749aefb51e450bf085e36
Shimaa Hassa <78687204+ShimaaHasan@users.noreply.github.com>
2021-06-03 06:59:44 +0200

    support SPM

--- Package.swift
+++ Package.swift
@@ -7,9 +7,28 @@ let package = Package(
     name: "SwiftSpinner",
     platforms: [.iOS(.v9), .tvOS(.v9)],
     products: [
-        .library(name: "SwiftSpinner", targets: ["SwiftSpinner"])
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "SwiftSpinner",
+            targets: ["SwiftSpinner"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .target(name: "SwiftSpinner")
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "SwiftSpinner",
+            dependencies: [],
+            path: "Sources",
+            exclude: [
+                "Package.swift",
+                "SwiftSpinner.podspec",
+                "README.md",
+                "CHANGELOG.md",
+                "LICENSE",
+            ]),
     ]
 )
